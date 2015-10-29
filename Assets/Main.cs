6de192c8f60d0837b2a82b1f3ba6c128a783ec41
Giuseppe Portelli <giuseppe.portelli@gmail.com>
2015-10-30 00:07:00 +0100

    First commit

---
+++ Assets/Main.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Main : MonoBehaviour
+{
+    public int lines;
+    int i = 1;
+
+    // Use this for initialization
+    void Start()
+    {
+        TestMessageTypes();
+        //Debug.Log(Screen.height);
+        //TestSize(lines);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Debug.Log(i++);
+        }
+    }
+
+    void TestSize(int lines)
+    {
+        for (; i <= lines; i++)
+        {
+            Debug.Log(i);
+        }
+    }
+
+    void TestMessageTypes()
+    {
+        Debug.Log("Log message...");
+        Debug.LogWarning("Warning message...");
+        Debug.LogError("Error message...");
+
+        int a = 0, b = 5;
+        int c = b / a;
+    }
+
+
+}
